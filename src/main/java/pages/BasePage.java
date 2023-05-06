149026ecf2762eae3004c469d7e341dafb47dce3
Leonardo Lanni <leonardo.lanni@e2ycommerce.com>
2023-05-06 14:21:21 +0100

    Beautify code

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,16 +1,17 @@
 package pages;
 
-import config.TestConfig;
 import org.openqa.selenium.WebDriver;
 
-public class BasePage {
-    //class containing everything that all the pages have
-    //it must be extended by any page in the POM
-    public WebDriver driver;
-    public TestConfig config;
+import config.TestConfig;
 
-    public BasePage(WebDriver driver, TestConfig config) {
-        this.driver = driver;
-        this.config = config;
-    }
+public class BasePage {
+	// class containing everything that all the pages have
+	// it must be extended by any page in the POM
+	public WebDriver driver;
+	public TestConfig config;
+
+	public BasePage(WebDriver driver, TestConfig config) {
+		this.driver = driver;
+		this.config = config;
+	}
 }
